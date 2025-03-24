6ee7ed5d4881db9d16e56deb82e5aa3d252fdc31
Magnusmajo <dralexis19003227@gmail.com>
2025-03-23 23:14:42 -0300

    Hello world

---
+++ say_hello.py
@@ -0,0 +1,15 @@
+"""Our goal here is to start saying 'Hello!' to this explendit world of Programming in Python"""
+"""
+This module contains a simple function to print a greeting message.
+
+The purpose of this script is to demonstrate a basic example of how to
+output a greeting message in Python. It serves as an introductory exercise
+for beginners to get familiar with Python programming.
+"""
+
+print("Hello, World!")
+
+"""You can also store The string "Hello, World!" into a variable and then call this variable in th print function"""
+
+new_string = "Hello, World!"
+print(new_string)
