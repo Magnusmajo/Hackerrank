0e23b92a9a6a3077eb5397bbee3bc99d7c912b91
Magnusmajo <dralexis19003227@gmail.com>
2025-03-23 23:43:27 -0300

    Rtithmetic numbers

---
+++ aritmetic.py
@@ -0,0 +1,24 @@
+"""
+The provided code stub reads two integers from STDIN, a and b. Add code to print seven lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+The fourth line contains the division of the two numbers (first / second).
+The fifth line contains the integer division of the two numbers (first // second).
+The sixth line contains the modulus of the two numbers (first % second).
+The seventh line contains the exponentiation of the two numbers (first ** second).
+"""
+
+# Read two integers from STDIN
+a = int(input())
+b = int(input())
+
+# Print the results of arithmetic operations
+print(a + b)  # Sum
+print(a - b)  # Difference
+print(a * b)  # Product
+print(a / b)  # Division
+print(a // b) # Integer Division
+print(a % b)  # Modulus
+print(a ** b) # Exponentiation
